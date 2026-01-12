1df5b5b1b45e98dbea432015ab73a41c9677e5e6
sadidtahsin <tamim.tm@gmail.com>
2026-01-12 13:49:47 +0600

    vercel.json added & in settings.py vercel  host added

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -142,4 +142,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
