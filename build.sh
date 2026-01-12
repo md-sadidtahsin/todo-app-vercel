e0e0ecefad06481f48cd0fbe35c02577a0a2bfdd
sadidtahsin <tamim.tm@gmail.com>
2026-01-12 13:40:18 +0600

    vercel.json added & in settings.py vercel  host added

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-# build.sh
+#!/bin/bash
 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --no-input --clear
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'example',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
