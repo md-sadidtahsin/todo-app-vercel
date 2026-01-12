ff3b5417d788ec71dc6ebd13305dd56bb1cda716
sadidtahsin <tamim.tm@gmail.com>
2026-01-12 14:06:44 +0600

    csrf

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -45,7 +45,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://subclimatic-sherri-nonprovocatively.ngrok-free.dev',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'https://todo-app-vercel-pi.vercel.app/',
+    'https://todo-app-vercel-pi.vercel.app',
 ]
 
 
